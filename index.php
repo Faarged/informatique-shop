82fe6a82582295b4d633bb48d22be4b1aa33c1e1
lilian <l.stoklosa@simplon-charleville.fr>
2019-03-25 16:02:35 +0100

    création des index

---
+++ index.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+	      <meta charset="UTF-8"/>
+        <!-- vitrine nouveautés qui peuvent se vendre ds le futur (pas de e-commerce) -->
+        <title>informatique-shop</title>
+        <link
