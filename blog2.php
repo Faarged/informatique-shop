369661f236ee1c5aa70cb2cb43bf368e286ac44b
lilian <l.stoklosa@simplon-charleville.fr>
2019-03-28 15:14:29 +0100

    lien et bouton actif

--- blog2.php
+++ blog2.php
@@ -153,8 +153,8 @@
             <div class="row bt">
                 <div class="col-sm-12">
                     <ul>
-                        <li><a class="active" href="blog.php">1</a></li>
-                        <li><a href="#">2</a></li>
+                        <li><a href="blog.php">1</a></li>
+                        <li><a class="active" href="blog2.php">2</a></li>
                         <li><a href="#">3</a></li>
                         <li><a href="#">4</a></li>
                         <li><a href="#">5</a></li>
